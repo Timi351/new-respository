9ce514ded664e247912e3a6ec5b9f504f03ecb51
Timi351 <you@example.com>
2024-01-22 02:27:52 +0100

    first commit

---
+++ src/MarvelData.js
@@ -0,0 +1,95 @@
+export const characters = [
+  // Iron Man
+
+  {
+    name: "Vegetable soup",
+    alias: "Efo riro",
+    img1: "https://media.istockphoto.com/id/1262255141/photo/nigerian-pounded-yam-served-with-afang-soup.jpg?s=612x612&w=0&k=20&c=-c67GKUQHuWcKHf6SR7M3JGC4CzZ6dwyG5cKGBRyFR0=",
+    homeworld: "Yoruba",
+    abilities: [
+      "It is a delicious delicacy eated with pounded yam,semo",
+    ],
+    team: "Southwest",
+    description:
+      "Efo Riro is a Yoruba spinach and tomato stew cooked with assorted meats or fish. It's flavored with a blend of spices and can be served with rice, pounded yam, or any other preferred side.",
+  },
+  {
+    name: "Banga soup",
+    alias: "Banga soup",
+    abilities: ["It is a delicious delicacy eated with pounded yam"],
+    team: "Southeast",
+    img1: "https://media.istockphoto.com/id/1327486579/photo/abak-soup-with-fufu-or-pounded-yam.jpg?s=612x612&w=0&k=20&c=i9WYYbJ4WeHdMXXiLFKFJL8yNyRQr8shPzsOAoc_SKo=",
+    homeworld: "Igbo",
+    description:
+      "  Banga soup, also known as Ofe Akwu, is a rich and spicy soup made from palm fruit extract, meat (usually goat meat or fish), and traditional spices. It is a delicacy in the Niger Delta region and is often served with starch, rice, or fufu..",
+  },
+  {
+    name: "pastry",
+    alias: "Puff puff",
+    abilities: ["It is a common pastry enjoyed on the go. taken with soft drink"],
+    team: "Lagos",
+    img1: "https://images.unsplash.com/photo-1665833613236-7c1d087463b1?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cHVmZiUyMHB1ZmZ8ZW58MHx8MHx8fDA%3D",
+    homeworld: "",
+
+    description:
+      "Puff-puff is a popular Nigerian street snack. It is a deep-fried dough ball made from a mixture of flour, sugar, yeast, and water. Puff-puff is soft and fluffy on the inside and slightly crispy on the outside .",
+  },
+  {
+    name: "Bean pudding",
+    alias: "Moi moi",
+    abilities: ["It is common among the lagosians. Eaten with bread or garri"],
+    team: "Lagos",
+    img1: "https://media.istockphoto.com/id/180867515/photo/vegetable-spread-on-knife.webp?b=1&s=170667a&w=0&k=20&c=Du-g2T8_lXwCLIcrBRetRgUFLUGW1chW14Z2G6iXjGU=",
+    homeworld: "",
+
+    description:
+      "Moimoi is a steamed bean pudding made from ground peeled beans, onions, peppers, and spices. It can be customized with ingredients like boiled eggs, fish, or vegetables. Moimoi is a nutritious and protein-rich dish.",
+  },
+
+  // Spider-Man
+  {
+    name: "Bean cake",
+    alias: "Akara",
+    abilities: ["It is common among the lagosians. Eaten with bread or garri"],
+    team: "Lagos",
+    img1: "https://images.unsplash.com/photo-1647162264554-5f60af27f052?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhbiUyMGNha2V8ZW58MHx8MHx8fDA%3D",
+    homeworld: "",
+    img3: "url_to_img3_for_Natasha_Stark",
+    description:
+      "Akara is a deep-fried snack made from ground black-eyed peas or beans, onions, and spices. The mixture is formed into balls and deep-fried until golden brown. Akara is commonly eaten as a breakfast item or snack.      .",
+  },
+
+  {
+    name: "Meat on a stick",
+    alias: "Suya",
+    abilities: ["Very spicy smoked meat"],
+    team: "Hausa",
+    img1: "https://media.istockphoto.com/id/643919286/photo/grilling-shashlik-on-barbecue-grill.jpg?s=612x612&w=0&k=20&c=WnaykfIIJqHsqxD8elki787QXTuMzBRhowzp5QZe8n0=",
+    homeworld: "",
+    description:
+      "Suya is a popular street food in Nigeria. It consists of skewered and grilled meat (usually beef or chicken) coated in a spicy peanut mix. Suya is often served with sliced onions, tomatoes, and spicy pepper sauce..",
+  },
+
+  {
+    name: "Pounded yam and  egusi soup",
+    alias: "Pounded yam and  egusi soup",
+    abilities: ["It is a very nutrious meal. The soup is garnished with meat,fish,kpomo."],
+    team: "Yoruba",
+    img1: "https://media.istockphoto.com/id/1398351215/photo/egusi-soup-with-fufu-pounded-yam.jpg?s=612x612&w=0&k=20&c=bJG20ZzU2NwfmFjCHbM7mVMaDj6fR7_nEuCSXClJiSE=",
+    homeworld: "",
+    description:
+      "Pounded yam is a starchy side dish made by boiling yams and then pounding them until smooth. It is often served with a thick soup, such as egusi soup, which is made with ground melon seeds, leafy vegetables, and meat or fish.      .",
+  },
+  {
+    name: "Party rice",
+    alias: "Jollof rice",
+    abilities: ["It is a very popular dish served on various ceremonies. It is enjoyed with salad,fruit."],
+    team: "",
+    img1: "https://media.istockphoto.com/id/1385278106/photo/west-african-entree-of-jollof-rice-fish-and-salad.jpg?s=612x612&w=0&k=20&c=wmj_CUt834SuhXGnI_auaYImK0oaB7bmPXqRZu5Ik3k=",
+    homeworld: "",
+    description:
+      "Jollof rice is a popular one-pot dish made with rice, tomatoes, onions, and a blend of spices. It is often cooked with vegetables, meat, or fish, and it's a staple at celebrations and gatherings..",
+  },
+];
+
+console.log(characters);
